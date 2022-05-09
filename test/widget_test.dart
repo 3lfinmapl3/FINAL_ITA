9db7cf6bdb91a514c84dc030d8631d0faecc4a33
3finmapl3 <elfinmaple0710@gmail.com>
2022-05-09 22:34:57 +0530

    Refactored the main.dart page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:income_tax_calculator/incomeTaxHomePage.dart';
+import 'package:income_tax_calculator/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
